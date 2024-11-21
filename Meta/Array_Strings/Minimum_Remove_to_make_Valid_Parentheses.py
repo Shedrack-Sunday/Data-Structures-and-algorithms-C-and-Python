a174614038f111947e1a75e5d8817acb90bc12f0
Shedrack Sunday <Hannania002@gmail.com>
2024-11-21 04:21:31 +0100

    Add Meta solutions

--- Meta/Array_Strings/Minimum_Remove_to_make_Valid_Parentheses.py
+++ Meta/Array_Strings/Minimum_Remove_to_make_Valid_Parentheses.py
@@ -33,3 +33,25 @@ s[i] is either '(' , ')', or lowercase English letter.
 
 
 """
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        res = []
+        count = 0
+
+        for char in s:
+            if char == '(':
+                res.append(char)
+                count += 1
+            elif char == ")" and count > 0:
+                res.append(char)
+                count -= 1
+            elif char != ')':
+                res.append(char)
+        
+        filtered = []
+        for char in res[::-1]:
+            if char == '(' and count > 0:
+                count -= 1
+            else:
+                filtered.append(char)
+        return "".join(filtered[::-1])
