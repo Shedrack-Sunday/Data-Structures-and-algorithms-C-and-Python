934b23812cef0de73b75fedce5c4254e70464c98
Shedrack Sunday <hannania002@gmail.com>
2025-01-01 13:48:36 +0100

    Create Valid_Anagram.py

---
+++ Neetcode/Arrays_Hashing/Valid_Anagram.py
@@ -0,0 +1,48 @@
+
+## Sorting 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        return sorted(s) == sorted(t)
+Time: O(nlogn + mlogm)
+space: O(1) or O(n + m)
+
+
+## Hash
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+
+Time: O(n + m)
+space: O(1)
+
+
+
+## Hash(Optimal)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+
+        for val in count:
+            if val != 0:
+                return False
+        return True
+
+Time: O(n + m)
+Space: O(1)
